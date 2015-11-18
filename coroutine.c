9f5ee68df7405f511090d49fa307fd812acb5e24
CodeSun <cscodesun@gmail.com>
2015-11-18 16:40:48 +0800

    [dev]add coroutine source code

---
+++ coroutine.c
@@ -0,0 +1,103 @@
+#include "coroutine.h"
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define PRIVATE static
+
+PRIVATE void __bridge() {
+	/* get the pointer to the keys */
+	void* zone;
+	zone = (void*)(((long)&zone + PAGE_SIZE - 1) / PAGE_SIZE * PAGE_SIZE);
+	zone -= sizeof(void*) * 12;
+	void** keys = (void**)zone;
+	Coroutine coro = (Coroutine)keys[0];
+
+MAIN_RUN:
+	coro->main(coro);
+END_AGAIN:
+	coro->state = CORO_END;
+	int ret = regsw(coro->env, 0);
+	switch(ret) {
+		case -1:
+			Coroutine_yield(coro);
+			goto MAIN_RUN;
+		default:
+	/* can not resume coroutine whose state is C_END */
+	/* this situation can be seen as a kind of error */
+			goto END_AGAIN;
+	}
+}
+
+Coroutine Coroutine_new(size_t size) {
+	if(size <= STK_DEFAULT_SIZE) {
+		size = STK_DEFAULT_SIZE;
+	} else {
+		size = (size + PAGE_SIZE - 1) / PAGE_SIZE * PAGE_SIZE;
+	}
+	void* stk = aligned_alloc(PAGE_SIZE, size);
+	Coroutine coro = (Coroutine)stk;
+	coro->state = CORO_INIT;
+	coro->stk = stk;
+	coro->bot = stk + GREEN_ZONE;
+	coro->top = stk + (STK_DEFAULT_SIZE - RED_ZONE);
+	return coro;
+}
+
+void Coroutine_bind(Coroutine coro, coro_cb_t main) {
+	if(Coroutine_isInit(coro) == 0) {
+		coro->main = main;
+		return;
+	}
+	if(setreg(coro->env)) {
+		__bridge();
+		/* NOTICE: pc NEVER point to this position */
+	}
+	coro->state = CORO_PEND;
+	coro->main = main;
+	coro->sp = coro->env[0];
+	coro->env[0] = coro->top;
+	void* keys = coro->stk + (STK_DEFAULT_SIZE - sizeof(void*) * 12);
+	((void**)keys)[0] = coro;
+}
+
+/* the following functions is used for DEBUG */
+/* formation is only ready for UNIX-like platform */
+void Coroutine_dumpRegs(Coroutine coro) {
+#if defined(__i386__)
+	fprintf(stderr, "[\e[1;32mSP\e[0m] %p\n", coro->env[0]);
+	fprintf(stderr, "[\e[1;32mBX\e[0m] %p\n", coro->env[1]);
+	fprintf(stderr, "[\e[1;32mSI\e[0m] %p\n", coro->env[2]);
+	fprintf(stderr, "[\e[1;32mDI\e[0m] %p\n", coro->env[3]);
+	fprintf(stderr, "[\e[1;32mBP\e[0m] %p\n", coro->env[4]);
+	fprintf(stderr, "[\e[1;32mPC\e[0m] %p\n", coro->env[5]);
+#elif defined(__amd64__) || defined(__x86_64__)
+	fprintf(stderr, "[\e[1;32mRSP\e[0m] %p\n", coro->env[0]);
+	fprintf(stderr, "[\e[1;32mRBX\e[0m] %p\n", coro->env[1]);
+	fprintf(stderr, "[\e[1;32mRBP\e[0m] %p\n", coro->env[2]);
+	fprintf(stderr, "[\e[1;32mR12\e[0m] %p\n", coro->env[3]);
+	fprintf(stderr, "[\e[1;32mR13\e[0m] %p\n", coro->env[4]);
+	fprintf(stderr, "[\e[1;32mR14\e[0m] %p\n", coro->env[5]);
+	fprintf(stderr, "[\e[1;32mR15\e[0m] %p\n", coro->env[6]);
+	fprintf(stderr, "[\e[1;32mRPC\e[0m] %p\n", coro->env[7]);
+#endif
+}
+
+#define STRIDE 32
+
+void Coroutine_dumpStack(Coroutine coro) {
+	void* p = coro->top - STRIDE;
+	int i;
+	while(p >= coro->bot) {
+		fprintf(stderr, "[\e[1;32m%p\e[0m]", p);
+		for(i = 1; i <= STRIDE; ++i) {
+			if(i % 4 == 1) {
+				fprintf(stderr, " %02hhX", *((unsigned char*)p + i));
+			} else {
+				fprintf(stderr, "%02hhX", *((unsigned char*)p + i));
+			}
+		}
+		fprintf(stderr, "\n");
+		p -= STRIDE;
+	}
+}
---
+++ coroutine.h
@@ -0,0 +1,86 @@
+/*
+ * Linux and FreeBSD compatible Coroutine implementation.
+ * Not ready for Windows currently.
+ */
+#ifndef _COROUTINE_H_
+#define _COROUTINE_H_
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+#define RED_ZONE 128
+#define GREEN_ZONE 512
+#define PAGE_SIZE 4096
+#define STK_DEFAULT_SIZE (64 * 1024)
+
+#if defined(__i386__)
+	#define RLEN 6
+#elif defined(__amd64__) || defined(__x86_64__)
+	#define RLEN 8
+#endif
+
+struct coroutine;
+
+typedef void (*coro_cb_t)(struct coroutine*);
+
+typedef enum cstat {
+	CORO_INIT,
+	CORO_PEND,
+	CORO_RUN,
+	CORO_END
+} cstat_t;
+
+typedef void* regbuf_t[RLEN];
+
+typedef struct coroutine {
+	cstat_t state;
+	regbuf_t env;
+	void* stk;
+	void* top;
+	void* bot;
+	void* sp;
+	coro_cb_t main;
+	void* res;
+} coroutine_t, *Coroutine;
+
+/* get or judge the status of coroutine */
+#define Coroutine_status(coro) (coro)->state
+#define Coroutine_isInit(coro) (coro)->state == CORO_INIT
+#define Coroutine_isPend(coro) (coro)->state == CORO_PEND
+#define Coroutine_isRun(coro) (coro)->state == CORO_RUN
+#define Coroutine_isEnd(coro) (coro)->state == CORO_END
+
+Coroutine Coroutine_new(size_t);
+
+/* coroutine cannot be closed when it is running */
+#define Coroutine_close(coro) \
+	assert(Coroutine_status(coro) != CORO_RUN); \
+	free((coro))
+
+/* bind the main function for coroutine */
+void Coroutine_bind(Coroutine, coro_cb_t);
+
+/* internal function of coroutine, do not use them */
+extern int  setreg(regbuf_t);
+extern int  regsw(regbuf_t, int);
+
+#define Coroutine_yield(coro)  \
+	(coro)->state = CORO_PEND; \
+	regsw((coro)->env, 0)
+
+#define Coroutine_resume(coro) \
+	(coro)->state = CORO_RUN;  \
+	regsw((coro)->env, 1)
+
+#define Coroutine_reset(coro)      \
+	assert(Coroutine_isEnd(coro)); \
+	regsw((coro)->env, -1)
+
+/* functions for Debug */
+#if defined(DEBUG)
+void Coroutine_dumpRegs(Coroutine);
+void Coroutine_dumpStack(Coroutine);
+#endif // DEBUG
+
+#endif // _COROUTINE_H_
